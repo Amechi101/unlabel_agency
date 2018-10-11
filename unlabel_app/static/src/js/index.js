1ed99419bbaf21fd168612586b6d0d641f3a8420
amechi101 <egbe@usc.edu>
2018-10-11 17:21:00 -0400

    console log change

--- unlabel_app/static/src/js/index.js
+++ unlabel_app/static/src/js/index.js
@@ -17,15 +17,17 @@ import LocomotiveScroll from './modules/ScrollManager'
 
 class App {
   constructor() {
-    // Signature
-    if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
-        const args = ['\n %c Built with <3, by Unlabel. https://unlabel.us/ \n\n', 'background: #DEEDEE; padding:5px 0; color: #232323;'];
-        
-        window.console.log.apply(console, args);
+    console.log('[Unlabel App]');
 
-    } else if (window.console) {
-        window.console.log('[Built with <3, by Unlabel.]');
-    }
+    // Signature
+    // if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
+    //     const args = ['\n %c Built with <3, by Unlabel. https://unlabel.us/ \n\n', 'background: #DEEDEE; padding:5px 0; color: #232323;'];
+        
+    //     window.console.log.apply(console, args);
+
+    // } else if (window.console) {
+    //     window.console.log('[Built with <3, by Unlabel.]');
+    // }
 
     this.browserCheck()
     this.initAnimations()

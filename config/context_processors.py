845eb3c6939907d52dd7632687b575bae79a6864
amechi101 <egbe@usc.edu>
2018-10-11 17:14:17 -0400

    production bool env set

--- config/context_processors.py
+++ config/context_processors.py
@@ -10,28 +10,6 @@ def site_processor(request):
 
 def consts(request):
     return dict(
-        ICON_EFFECTS = dict(
-            format="png",
-            type="facebook",
-            transformation=[
-                dict(height=95, width=95, crop="thumb", gravity="face", effect="sepia", radius=20),
-                dict(angle=10),
-            ]
-        ),
-        COMPRESS_IMAGES = dict(
-            format="jpg", 
-            type="fetch",
-            crop="fit", 
-            height=1000, 
-            width=1000,
-            quality="auto"
-        ),
-        THUMBNAIL = dict(
-            format="jpg", 
-            crop="fill", 
-            height=284, 
-            width=284,
-        ),
         GENERAL_IMAGES = dict(
             format="jpg", 
             transformation=[
--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 from .base_settings import *
 
-PRODUCTION = True
+PRODUCTION = env.bool('DJANGO_PRODUCTION_BOOL', default=False)
 
 if PRODUCTION == True:
     from .production_settings import *
